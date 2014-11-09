964568c7a7fa13aa754c0f0c21a566581be84d25
Aleksey Kuznetsov <zummenix@gmail.com>
2014-11-09 21:46:31 +0500

    Extracted ease functions as global functions.

--- src/ease.rs
+++ src/ease.rs
@@ -50,179 +50,282 @@ pub enum EaseFunction {
 
 impl EaseFunction {
     /// Calculate the eased value, normalized
-    pub fn calc(self, mut p: f64) -> f64 {
-        if p > 1.0 {
-            p = 1.0;
-        } else if p < 0.0 {
-            p = 0.0;
-        }
-
+    pub fn calc(self, p: f64) -> f64 {
         match self {
-            EaseQuadraticIn => {
-                p * p
-            },
-            EaseQuadraticOut => {
-                -(p * (p - 2.0))
-            },
-            EaseQuadraticInOut => {
-                if p < 0.5 {
-                    2.0 * p * p
-                } else {
-                    (-2.0 * p * p) + (4.0 * p) - 1.0
-                }
-            },
+            EaseQuadraticIn => quadratic_in(p),
+            EaseQuadraticOut => quadratic_out(p),
+            EaseQuadraticInOut => quadratic_in_out(p),
 
-            EaseCubicIn => {
-                p * p * p
-            },
-            EaseCubicOut => {
-                let f = p - 1.0;
-                f * f * f + 1.0
-            },
-            EaseCubicInOut => {
-                if p < 0.5 {
-                    4.0 * p * p * p
-                } else {
-                    let f = (2.0 * p) - 2.0;
-                    0.5 * f * f * f + 1.0
-                }
-            },
+            EaseCubicIn => cubic_in(p),
+            EaseCubicOut => cubic_out(p),
+            EaseCubicInOut => cubic_in_out(p),
 
-            EaseQuarticIn => {
-                p * p * p * p
-            },
-            EaseQuarticOut => {
-                let f = p - 1.0;
-                f * f * f * (1.0 - p) + 1.0
-            },
-            EaseQuarticInOut => {
-                if p < 0.5 {
-                    8.0 * p * p * p * p
-                } else {
-                    let f = p - 1.0;
-                    -8.0 * f * f * f * f + 1.0
-                }
-            },
+            EaseQuarticIn => quartic_in(p),
+            EaseQuarticOut => quartic_out(p),
+            EaseQuarticInOut => quartic_in_out(p),
 
-            EaseQuinticIn => {
-                p * p * p * p *p
-            },
-            EaseQuinticOut => {
-                let f = p - 1.0;
-                f * f * f * f * f + 1.0
-            },
-            EaseQuinticInOut => {
-                if p < 0.5  {
-                    16.0 * p * p * p * p * p
-                } else {
-                    let f = (2.0 * p) - 2.0;
-                    0.5 * f * f * f * f * f + 1.0
-                }
-            },
+            EaseQuinticIn => quintic_in(p),
+            EaseQuinticOut => quintic_out(p),
+            EaseQuinticInOut => quintic_in_out(p),
 
-            EaseSineIn => {
-                ((p - 1.0) * PI_2).sin() + 1.0
-            },
-            EaseSineOut => {
-                (p * PI_2).sin()
-            },
-            EaseSineInOut => {
-                0.5 * (1.0 - (p * PI).cos())
-            },
+            EaseSineIn => sine_in(p),
+            EaseSineOut => sine_out(p),
+            EaseSineInOut => sine_in_out(p),
 
-            EaseCircularIn => {
-                1.0 - (1.0 - (p * p)).sqrt()
-            },
-            EaseCircularOut => {
-                ((2.0 - p) * p).sqrt()
-            },
-            EaseCircularInOut => {
-                if p < 0.5 {
-                    0.5 * (1.0 - (1.0 - 4.0 * (p * p)).sqrt())
-                } else {
-                    0.5 * ((-((2.0 * p) - 3.0) * ((2.0 * p) - 1.0)).sqrt() + 1.0)
-                }
-            },
+            EaseCircularIn => circular_in(p),
+            EaseCircularOut => circular_out(p),
+            EaseCircularInOut => circular_in_out(p),
 
-            EaseExponentialIn => {
-                if p == 0.0 {
-                    p
-                } else {
-                    2.0f64.powf(10.0 * (p - 1.0))
-                }
-            },
-            EaseExponentialOut => {
-                if p == 1.0 {
-                    p
-                } else {
-                    1.0 - 2.0f64.powf(-10.0 * p)
-                }
-            },
-            EaseExponentialInOut => {
-                if p == 0.0 || p == 1.0 {
-                    return p;
-                }
+            EaseExponentialIn => exponential_in(p),
+            EaseExponentialOut => exponential_out(p),
+            EaseExponentialInOut => exponential_in_out(p),
 
-                if p < 0.5  {
-                    0.5 * 2.0f64.powf((20.0 * p) - 10.0)
-                } else {
-                    -0.5 * 2.0f64.powf((-20.0 * p) + 10.0) + 1.0
-                }
-            },
+            EaseElasticIn => elastic_in(p),
+            EaseElasticOut => elastic_out(p),
+            EaseElasticInOut => elastic_in_out(p),
 
-            EaseElasticIn => {
-                (13.0 * PI_2 * p).sin() * 2.0f64.powf(10.0 * (p - 1.0))
-            },
-            EaseElasticOut => {
-                (-13.0 * PI_2 * (p + 1.0)).sin() * 2.0f64.powf(-10.0 * p) + 1.0
-            },
-            EaseElasticInOut => {
-                if p < 0.5 {
-                    0.5 * (13.0 * PI_2 * (2.0 * p)).sin() * 2.0f64.powf(10.0 * ((2.0 * p) - 1.0))
-                } else {
-                    0.5 * ((-13.0 * PI_2 * ((2.0 * p - 1.0) + 1.0)).sin() * 2.0f64.powf(-10.0 * (2.0 * p - 1.0)) + 2.0)
-                }
-            },
+            EaseBackIn => back_in(p),
+            EaseBackOut => back_out(p),
+            EaseBackInOut => back_in_out(p),
 
-            EaseBackIn => {
-                p * p * p - p * (p * PI).sin()
-            },
-            EaseBackOut => {
-                let f = 1.0 - p;
-                1.0 - (f * f * f - f * (f * PI).sin())
-            },
-            EaseBackInOut => {
-                if p < 0.5 {
-                    let f = 2.0 * p;
-                    0.5 * (f * f * f - f * (f * PI).sin())
-                } else {
-                    let f = 1.0 - (2.0 * p - 1.0);
-                    0.5 * (1.0 - (f * f * f - f * (f * PI).sin())) + 0.5
-                }
-            },
-
-            EaseBounceIn => {
-                1.0 - EaseBounceOut.calc(1.0 - p)
-            },
-            EaseBounceOut => {
-                if p < 4.0 / 11.0 {
-                    (121.0 * p * p) / 16.0
-                } else if p < 8.0 / 11.0 {
-                    (363.0 / 40.0 * p * p) - (99.0 / 10.0 * p) + 17.0 / 5.0
-                } else if p < 9.0 / 10.0 {
-                    (4356.0 / 361.0 * p * p) - (35442.0 / 1805.0 * p) + 16061.0 / 1805.0
-                } else {
-                    (54.0 / 5.0 * p * p) - (513.0 / 25.0 * p) + 268.0 / 25.0
-                }
-            },
-            EaseBounceInOut => {
-                if p < 0.5 {
-                    0.5 * EaseBounceIn.calc(p * 2.0)
-                } else {
-                    0.5 * EaseBounceOut.calc(p * 2.0 - 1.0) + 0.5
-                }
-            },
+            EaseBounceIn => bounce_in(p),
+            EaseBounceOut => bounce_out(p),
+            EaseBounceInOut => bounce_in_out(p),
         }
     }
 }
 
+
+pub fn quadratic_in(mut p: f64) -> f64 {
+    p = normalized(p);
+    p * p
+}
+
+pub fn quadratic_out(mut p: f64) -> f64 {
+    p = normalized(p);
+    -(p * (p - 2.0))
+}
+
+pub fn quadratic_in_out(mut p: f64) -> f64 {
+    p = normalized(p);
+    if p < 0.5 {
+        2.0 * p * p
+    } else {
+        (-2.0 * p * p) + (4.0 * p) - 1.0
+    }
+}
+
+
+pub fn cubic_in(mut p: f64) -> f64 {
+    p = normalized(p);
+    p * p * p
+}
+
+pub fn cubic_out(mut p: f64) -> f64 {
+    p = normalized(p);
+    let f = p - 1.0;
+    f * f * f + 1.0
+}
+
+pub fn cubic_in_out(mut p: f64) -> f64 {
+    p = normalized(p);
+    if p < 0.5 {
+        4.0 * p * p * p
+    } else {
+        let f = (2.0 * p) - 2.0;
+        0.5 * f * f * f + 1.0
+    }
+}
+
+
+pub fn quartic_in(mut p: f64) -> f64 {
+    p = normalized(p);
+    p * p * p * p
+}
+
+pub fn quartic_out(mut p: f64) -> f64 {
+    p = normalized(p);
+    let f = p - 1.0;
+    f * f * f * (1.0 - p) + 1.0
+}
+
+pub fn quartic_in_out(mut p: f64) -> f64 {
+    p = normalized(p);
+    if p < 0.5 {
+        8.0 * p * p * p * p
+    } else {
+        let f = p - 1.0;
+        -8.0 * f * f * f * f + 1.0
+    }
+}
+
+
+pub fn quintic_in(mut p: f64) -> f64 {
+    p = normalized(p);
+    p * p * p * p * p
+}
+
+pub fn quintic_out(mut p: f64) -> f64 {
+    p = normalized(p);
+    let f = p - 1.0;
+    f * f * f * f * f + 1.0
+}
+
+pub fn quintic_in_out(mut p: f64) -> f64 {
+    p = normalized(p);
+    if p < 0.5  {
+        16.0 * p * p * p * p * p
+    } else {
+        let f = (2.0 * p) - 2.0;
+        0.5 * f * f * f * f * f + 1.0
+    }
+}
+
+
+pub fn sine_in(mut p: f64) -> f64 {
+    p = normalized(p);
+    ((p - 1.0) * PI_2).sin() + 1.0
+}
+
+pub fn sine_out(mut p: f64) -> f64 {
+    p = normalized(p);
+    (p * PI_2).sin()
+}
+
+pub fn sine_in_out(mut p: f64) -> f64 {
+    p = normalized(p);
+    0.5 * (1.0 - (p * PI).cos())
+}
+
+
+pub fn circular_in(mut p: f64) -> f64 {
+    p = normalized(p);
+    1.0 - (1.0 - (p * p)).sqrt()
+}
+
+pub fn circular_out(mut p: f64) -> f64 {
+    p = normalized(p);
+    ((2.0 - p) * p).sqrt()
+}
+
+pub fn circular_in_out(mut p: f64) -> f64 {
+    p = normalized(p);
+    if p < 0.5 {
+        0.5 * (1.0 - (1.0 - 4.0 * (p * p)).sqrt())
+    } else {
+        0.5 * ((-((2.0 * p) - 3.0) * ((2.0 * p) - 1.0)).sqrt() + 1.0)
+    }
+}
+
+
+pub fn exponential_in(mut p: f64) -> f64 {
+    p = normalized(p);
+    if p == 0.0 {
+        p
+    } else {
+        2.0f64.powf(10.0 * (p - 1.0))
+    }
+}
+
+pub fn exponential_out(mut p: f64) -> f64 {
+    p = normalized(p);
+    if p == 1.0 {
+        p
+    } else {
+        1.0 - 2.0f64.powf(-10.0 * p)
+    }
+}
+
+pub fn exponential_in_out(mut p: f64) -> f64 {
+    p = normalized(p);
+    if p == 0.0 || p == 1.0 {
+        return p;
+    }
+
+    if p < 0.5  {
+        0.5 * 2.0f64.powf((20.0 * p) - 10.0)
+    } else {
+        -0.5 * 2.0f64.powf((-20.0 * p) + 10.0) + 1.0
+    }
+}
+
+
+pub fn elastic_in(mut p: f64) -> f64 {
+    p = normalized(p);
+    (13.0 * PI_2 * p).sin() * 2.0f64.powf(10.0 * (p - 1.0))
+}
+
+pub fn elastic_out(mut p: f64) -> f64 {
+    p = normalized(p);
+    (-13.0 * PI_2 * (p + 1.0)).sin() * 2.0f64.powf(-10.0 * p) + 1.0
+}
+
+pub fn elastic_in_out(mut p: f64) -> f64 {
+    p = normalized(p);
+    if p < 0.5 {
+        0.5 * (13.0 * PI_2 * (2.0 * p)).sin() * 2.0f64.powf(10.0 * ((2.0 * p) - 1.0))
+    } else {
+        0.5 * ((-13.0 * PI_2 * ((2.0 * p - 1.0) + 1.0)).sin() * 2.0f64.powf(-10.0 * (2.0 * p - 1.0)) + 2.0)
+    }
+}
+
+
+pub fn back_in(mut p: f64) -> f64 {
+    p = normalized(p);
+    p * p * p - p * (p * PI).sin()
+}
+
+pub fn back_out(mut p: f64) -> f64 {
+    p = normalized(p);
+    let f = 1.0 - p;
+    1.0 - (f * f * f - f * (f * PI).sin())
+}
+
+pub fn back_in_out(mut p: f64) -> f64 {
+    p = normalized(p);
+    if p < 0.5 {
+        let f = 2.0 * p;
+        0.5 * (f * f * f - f * (f * PI).sin())
+    } else {
+        let f = 1.0 - (2.0 * p - 1.0);
+        0.5 * (1.0 - (f * f * f - f * (f * PI).sin())) + 0.5
+    }
+}
+
+
+pub fn bounce_in(mut p: f64) -> f64 {
+    p = normalized(p);
+    1.0 - bounce_out(1.0 - p)
+}
+
+pub fn bounce_out(mut p: f64) -> f64 {
+    p = normalized(p);
+    if p < 4.0 / 11.0 {
+        (121.0 * p * p) / 16.0
+    } else if p < 8.0 / 11.0 {
+        (363.0 / 40.0 * p * p) - (99.0 / 10.0 * p) + 17.0 / 5.0
+    } else if p < 9.0 / 10.0 {
+        (4356.0 / 361.0 * p * p) - (35442.0 / 1805.0 * p) + 16061.0 / 1805.0
+    } else {
+        (54.0 / 5.0 * p * p) - (513.0 / 25.0 * p) + 268.0 / 25.0
+    }
+}
+
+pub fn bounce_in_out(mut p: f64) -> f64 {
+    p = normalized(p);
+    if p < 0.5 {
+        0.5 * bounce_in(p * 2.0)
+    } else {
+        0.5 * bounce_out(p * 2.0 - 1.0) + 0.5
+    }
+}
+
+fn normalized(p: f64) -> f64 {
+    if p > 1.0 {
+        1.0
+    } else if p < 0.0 {
+        0.0
+    } else {
+        p
+    }
+}
